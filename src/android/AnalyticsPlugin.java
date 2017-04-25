20ccef2e69cec6d63f6773b4fe8d1b3d7dc49461
Connor Eggleston <connoreggleston@outlook.com>
2017-04-25 16:45:55 -0700

    Update AnalyticsPlugin.java

--- src/android/AnalyticsPlugin.java
+++ src/android/AnalyticsPlugin.java
@@ -49,21 +49,14 @@ public class AnalyticsPlugin extends CordovaPlugin {
             analytics = null;
             Log.e(TAG, "Invalid write key: " + writeKey);
         } else {
-            try {
-                Log.e(TAG, "Initializing Segment Analytics " + writeKey);
-                analytics = new Analytics.Builder(
-                    cordova.getActivity().getApplicationContext(),
-                    writeKey
-                ).logLevel(logLevel).build();
-                Log.e(TAG, "Analytics set to: " + analytics);
-
-                //Analytics.setSingletonInstance(analytics);
-            } catch (Exception e) {
-                Log.e(TAG, "Segment Error " + e.getMessage());
-                Log.e(TAG, "Segment Analytics already set " + writeKey);
-                Log.e(TAG, "Analytics alreadt set to: " + analytics);
-            }
+            Log.e(TAG, "Initializing Segment Analytics " + writeKey);
+            analytics = new Analytics.Builder(
+                cordova.getActivity().getApplicationContext(),
+                writeKey
+            ).logLevel(logLevel).build();
+            Log.e(TAG, "Analytics set to: " + analytics);
             
+            Analytics.setSingletonInstance(analytics);
         }
     }
 
